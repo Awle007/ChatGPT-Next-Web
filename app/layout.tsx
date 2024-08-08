4e2091f0a3e3c92ab2b4d21f7e4210337d862264
Awle_ <141636413+Awle007@users.noreply.github.com>
2024-08-08 19:21:22 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "XSH云OpenAi",
+  description: "Powerd By XSH",
   appleWebApp: {
-    title: "NextChat",
+    title: "XSH云OpenAi",
     statusBarStyle: "default",
   },
 };
