5ef8e083edd3fbe8061b58e1cae05bbc2083435a
Awle_ <141636413+Awle007@users.noreply.github.com>
2024-08-08 23:38:49 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -7,8 +7,8 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized: isApp
-      ? "检测到无效 API Key，请前往[设置](/#/settings)页检查 API Key 是否配置正确，如有需要请[联系站长](/https://jc.xshyun.top/?id=5)。"
-      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码。本项目非免费使用，如有需要请[联系站长](/https://jc.xshyun.top/?id=5)",
+      ? "检测到无效 API Key，请前往[设置](/#/settings)页检查 API Key 是否配置正确，如有需要请[联系站长](https://jc.xshyun.top/?id=5)。"
+      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码。本项目非免费使用，如有需要请[联系站长](https://jc.xshyun.top/?id=5)",
   },
   Auth: {
     Title: "需要密码",
